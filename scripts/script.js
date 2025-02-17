9a6d597044cbe082f98e78c36d9b1ad62538b101
Lisa-Ni <niklasson.lisa@gmail.com>
2025-02-17 15:56:06 +0100

    random trailer

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+import { fetchTopMovies } from "./modules/api.js";
+import { renderTrailers } from "./modules/caroussel.js";
+
 
 if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
     console.log('index.html');
@@ -10,5 +13,16 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
 
 } else if(window.location.pathname === '/search.html') {
     console.log('search.html');
-
 }
+
+const topMovieList = await fetchTopMovies();
+ 
+function getRandomTrailers(trailers) {
+    const scrambled = trailers.sort(() => Math.random() - 0.5);
+    return scrambled.slice(0, 5);
+}
+
+let trailers = getRandomTrailers(topMovieList);
+
+for(let index in trailers){
+    renderTrailers(trailers[index], index)}
