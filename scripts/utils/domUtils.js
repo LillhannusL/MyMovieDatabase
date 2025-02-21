196125d82a8dbc477dde9273f7f281b45370cf52
Lisa-Ni <niklasson.lisa@gmail.com>
2025-02-21 13:21:23 +0100

    recomended

--- scripts/utils/domUtils.js
+++ scripts/utils/domUtils.js
@@ -1,2 +1,1 @@
-
-
+// import { topMovieList } from "../script.js";
---
+++ scripts/utils/utils.js
@@ -0,0 +1,5 @@
+import { fetchOMDBMovies } from "../modules/api.js";
+import { createMoviePosters } from "./domUtils.js"
+import { createMovieCard } from "../components/movieCard.js"
+
+
