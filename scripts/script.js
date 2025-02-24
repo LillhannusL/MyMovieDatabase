8b74b487ad3331dece88aae47a10c9095a0c48a6
Lisa-Ni <niklasson.lisa@gmail.com>
2025-02-24 15:43:03 +0100

    search and moviecards

--- scripts/script.js
+++ scripts/script.js
@@ -32,22 +32,26 @@ export async function fetchTopMovies() {
 }
 
 export async function fetchOMDBMovies(input) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=de7c82e1&s=${input}*`);
-    let data = await response.json();
-    console.log(data)
-
-    // const idfrominput = data.Search && data.Search[0].imdbID;
-    // if(idfrominput) {
-    // await fetchOMDBMovieDetails(idfrominput);
-    // }
-    return data;
+// 	const response = await fetch(`https://www.omdbapi.com/?apikey=de7c82e1&s=${encodeURIComponent(input)}`);
+//        if (!response.ok) {
+//            throw new Error('Nätverksrespons var inte OK');
+//        }
+//        return await response.json();
+//    }
+	try {
+		const response = await fetch(`http://www.omdbapi.com/?apikey=de7c82e1&s=${encodeURIComponent(input)}*`);
+		let data = await response.json();
+		return data;
+	} catch {
+		console.log(error.message);
+	}
 }
 
 export async function fetchOMDBMovieDetails(imdbID) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=de7c82e1&s&plot=full&i=${imdbID}`);
+    const response = await fetch(`http://www.omdbapi.com/?apikey=de7c82e1&s&plot=full&i=${encodeURIComponent(imdbID)}`);
     let data = await response.json();
-    console.log(data)
     return data;
+  
 }
 
 
@@ -64,18 +68,50 @@ async function pageSetup() {
 	renderMoviePosters(topMovieList);
     listenForFavorites()
 
-	// setupSearchForm();
+	setupSearchForm();
 }
 
-function searchPageSetup() {
-	renderMoviePosters();
-}
+async function searchPageSetup() {
+	console.log('sök')
+	const params = new URLSearchParams(window.location.search);
+	const searchQuery = params.get('movie');
+	console.log(searchQuery);
+
+
+	if(searchQuery) {
+		try	{
+			const data = await fetchOMDBMovies(searchQuery);
+			console.log(data); 
+			if(data.Search && data.Search.length > 0) {
+				console.log('filmer')
+				renderMoviePosters(data.Search);
+			}	else {
+                console.log('Inga filmer hittades');
+			}
+		} catch (error) {
+			console.error('Något gick fel vid hämtningen av filmer');
+		}
+	}
+};
 
 async function moviePageSetup() {
-	console.log(value);
+	console.log("movie page");
+    const params = new URLSearchParams(window.location.search);
+    const imdbID = decodeURIComponent(params.get('movie'));
 
-	const poster = renderMovieCard();
-	document.querySelector('movieInformation').appendChild(poster);
+    console.log(imdbID);
+
+    if(imdbID) {
+        const movie = await fetchOMDBMovieDetails(imdbID);
+        if(movie){
+            console.log(movie);
+        }
+    } else {
+        console.log('No imdbID found in URL')
+    }
+
+	await renderMovieCard(imdbID);
+	document.querySelector('#movieInformation').appendChild(poster);
 }
 
 function getRandomTrailers(trailers) {
@@ -86,7 +122,11 @@ function getRandomTrailers(trailers) {
 // Movie Posters on start page
 
 export function renderMoviePosters(movieList) {
+	console.log('Typ av movieList:', typeof movieList);
+	console.log('Innehåll av movieList:', movieList);
+	
     const posterRef = document.querySelector('#cardContainer')
+	posterRef.innerHTML = ''
     
     for(let movie of movieList) {
         let moviePoster = createMoviePosters(movie)
@@ -94,12 +134,28 @@ export function renderMoviePosters(movieList) {
     }
 }
 
-export function renderMovieCard(imdbID) {
+export async function renderMovieCard(imdbID) {
     const cardRef = document.querySelector('#movieInformation');
-    let movieCard = createMovieCard(imdbID);
+    const movie = await fetchOMDBMovieDetails(imdbID)
+
+    let movieCard = createMovieCard(movie);
     cardRef.appendChild(movieCard);    
 }
 
+async function setupSearchForm() {
+	const inputRef = document.querySelector('#searchInput');
+    const formRef = document.querySelector('#searchForm');
+
+	inputRef.addEventListener('input', (event) => {
+		console.log(event.target.value);
+	})
+
+	formRef.addEventListener('submit', async (event) => {
+		event.preventDefault();
+		await fetchOMDBMovies(inputRef.value)
+        window.location.href = `../search.html?movie=${inputRef.value}`
+	})
+}
 
 export function listenForFavorites() {
     let favoritesArray = [];
@@ -107,22 +163,28 @@ export function listenForFavorites() {
     const favorites = document.querySelectorAll('.favorites');
     favorites.forEach(favorite => {
         favorite.addEventListener('click', (event) => {
-
             event.target.classList.toggle('fa-regular');
             event.target.classList.toggle('fa-solid');
 
-            if(event.target.classList.contains('fa-solid')) {
-                console.log('Saving favorite', favoritesArray);
+            const imdbID = event.target.dataset.imdbID
+            const isFavorite =  fetchOMDBMovies(imdbID);
 
-            } else {
-                console.log('Removing favorite', favoritesArray);
-                              
-            }
+            if(isFavorite) {
+                const index = favoritesArray.indexOf(imdbID);
+                if(index >-1){
+                  favoritesArray.splice(index, 1)
+                  console.log('Removing favorite', favoritesArray)
+                } else {
+                  favoritesArray.push(imdbID);
+                  console.log('Saving Favorite', favoritesArray)
+                }
+                  }  
         })
     })
 }
 
-function addMovieToFavorites(movie) {
+
+function addMovieToFavorites(movieId) {
     // Lägg till filmen i en favoritlista (exempelvis i localStorage eller en global array)
     let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
     favorites.push(movie);
@@ -139,33 +201,11 @@ function removeMovieFromFavorites(movieId) {
 }
 
 
-// export async function setupSearchForm(movies) {
-//     const inputRef = document.querySelector('#searchInput');
-//     const formRef = document.querySelector('#searchForm');
-
-//     inputRef.addEventListener('input', (event) => {
-//             console.log(event.target.value)
-//         });
-
-//     formRef.addEventListener('submit', async (event) => { 
-//         event.preventDefault();
-//         const input = searchInput.value.trim();
-//         if (input) {
-//         const data = await fetchOMDBMovies(input);
-//         console.log(data);
-//         const jsonData = JSON.stringify(data)
-//         const encodeData = encodeURIComponent(jsonData)
-//         window.location.href = `./search.html?movie=${encodeData}`
-//         }
-//     })
-// }
-
-
 export function createMoviePosters(movie) {
     let posterRef = document.createElement('article');
     posterRef.classList.add('moviePoster')
     const posterTemp = `
-        <a class="movieLink" href="./movie.html?movie=${movie.Title}
+        <a class="movieLink" href="../movie.html?movie=${encodeURIComponent(movie.imdbID)}">
             <div class ="moviePoster__top">
                 <img
                 src="${movie.Poster}"
@@ -190,30 +230,33 @@ export function createMovieCard(movie) {
     let cardRef = document.createElement('article');
     cardRef.classList.add('movieCard')
     const cardTemp = `
-        <a href="./movie.html?movie=${movie.Title}
             <div class ="movieCard__top">
                 <img
                 src="${movie.Poster}"
                 alt="Poster of ${movie.Title}"
-                class="movieCard__Posters"
+                class="movieCard__Poster"
                 />
             </div>
-            <div class="moviePoster_bottom"
-                <p class="movieCard__info">${movie.Title}</p>
-                <p class="movieCard__info">Director: ${movie.Director}</p>
-                <p class="movieCard__info">Writer: ${movie.Writer}</p>
-                <p class="movieCard__info">Starring: ${movie.Actors}</p>
-                <p class="movieCard__info">Plot: ${movie.Plot}</p>
-                <p class="movieCard__info">Rated: ${movie.Rated}</p>
-                <p class="movieCard__info">Genre: ${movie.Genre}</p>
-                <p class="movieCard__info">Runtime: ${movie.Runtime}</p>
-                <p class="movieCard__info">Released: ${movie.Released}</p>
-                <p class="movieCard__info">Imdb-Rating: ${movie.imdbRating}</p>
+            <div class="moviePoster_bottom">
+            <p class="movieCard__info--title">${movie.Title}</p>
+            <div class="movieCard__info--artists">
+                <p class="movieCard__info"><b>Director:</b> <br>${movie.Director}</p>
+                <p class="movieCard__info"><b>Writer:</b> <br>${movie.Writer}</p>
+                <p class="movieCard__info"><b>Starring:</b> <br>${movie.Actors}</p>
             </div>
-        </a>       
+            <p class="movieCard__info"><b>Plot:</b> <br>${movie.Plot}</p>
+            <div class="movieCard__info--artists">
+                <p class="movieCard__info"><b>Rated:</b> <br>${movie.Rated}</p>
+                <p class="movieCard__info"><b>Genre:</b> <br>${movie.Genre}</p>
+                <p class="movieCard__info"><b>Runtime:</b> <br>${movie.Runtime}</p>
+                <p class="movieCard__info"><b>Released:</b> <br>${movie.Released}</p>
+                <p class="movieCard__info"><b>Imdb-Rating:</b> <br>${movie.imdbRating}</p>
+            </div>
+            </div>     
     `;
     cardRef.innerHTML = cardTemp;
 
     return cardRef;
-    // posterRef.src = poster;
 }
+
+
